e720ec2694e097dc06a35461927f70b86754b170
houcheng <houcheng@boco.com.cn>
2023-08-04 17:54:55 +0800

    熟悉kafkajs常用操作及rsuitejs

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './samples/node-api'
-import './index.scss'
+// import './index.scss'
+import Test from "@/test";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
 )
 
