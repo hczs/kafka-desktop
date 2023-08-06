7b4c8901cb718878fcf6eab9f0d494cf5e4fd5ae
sunny <hczshd@163.com>
2023-08-06 22:16:21 +0800

    :sparkles: Using electron-store to support data persistence

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,7 +1,8 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { release } from 'node:os'
-import { join } from 'node:path'
-import { update } from './update'
+import {app, BrowserWindow, shell, ipcMain} from 'electron'
+import Store from "electron-store";
+import {release} from 'node:os'
+import {join} from 'node:path'
+import {update} from './update'
 
 // The built directory structure
 //
@@ -16,8 +17,8 @@ import { update } from './update'
 process.env.DIST_ELECTRON = join(__dirname, '../')
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, '../public')
-  : process.env.DIST
+    ? join(process.env.DIST_ELECTRON, '../public')
+    : process.env.DIST
 
 // Disable GPU Acceleration for Windows 7
 if (release().startsWith('6.1')) app.disableHardwareAcceleration()
@@ -26,8 +27,8 @@ if (release().startsWith('6.1')) app.disableHardwareAcceleration()
 if (process.platform === 'win32') app.setAppUserModelId(app.getName())
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+    app.quit()
+    process.exit(0)
 }
 
 // Remove electron security warnings
@@ -41,81 +42,104 @@ const preload = join(__dirname, '../preload/index.js')
 const url = process.env.VITE_DEV_SERVER_URL
 const indexHtml = join(process.env.DIST, 'index.html')
 
+// electron-store 存储
+let store = new Store();
+
 async function createWindow() {
-  win = new BrowserWindow({
-    title: 'Main window',
-    icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
-    webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
+    win = new BrowserWindow({
+        title: 'Main window',
+        icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
+        webPreferences: {
+            preload,
+            // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+            // Consider using contextBridge.exposeInMainWorld
+            // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        frame: false
+    })
 
-  if (url) { // electron-vite-vue#298
-    win.loadURL(url)
-    // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
-  } else {
-    win.loadFile(indexHtml)
-  }
+    if (url) { // electron-vite-vue#298
+        win.loadURL(url)
+        // Open devTool if the app is not packaged
+        win.webContents.openDevTools()
+        console.log("open openDevTools")
+    } else {
+        win.loadFile(indexHtml)
+    }
 
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
+    // Test actively push message to the Electron-Renderer
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
 
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
+    // Make all links open with the browser, not with the application
+    win.webContents.setWindowOpenHandler(({url}) => {
+        if (url.startsWith('https:')) shell.openExternal(url)
+        return {action: 'deny'}
+    })
 
-  // Apply electron-updater
-  update(win)
+    // Apply electron-updater
+    update(win)
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
+    win = null
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
-  }
+    if (win) {
+        // Focus on the main window if the user tried to open another
+        if (win.isMinimized()) win.restore()
+        win.focus()
+    }
 })
 
 app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
-  if (allWindows.length) {
-    allWindows[0].focus()
-  } else {
-    createWindow()
-  }
+    const allWindows = BrowserWindow.getAllWindows()
+    if (allWindows.length) {
+        allWindows[0].focus()
+    } else {
+        createWindow()
+    }
 })
 
 // New window example arg: new windows url
 ipcMain.handle('open-win', (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
+    const childWindow = new BrowserWindow({
+        webPreferences: {
+            preload,
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
 
-  if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`)
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
-  }
+    if (process.env.VITE_DEV_SERVER_URL) {
+        childWindow.loadURL(`${url}#${arg}`)
+    } else {
+        childWindow.loadFile(indexHtml, {hash: arg})
+    }
 })
 
+const storePath = app.getPath("userData");
+ipcMain.on("saveData", (_,k , v) => {
+    store.set(k, v);
+    console.log('store result: ', store.get(k));
+    console.log("store path: ", storePath);
+})
+
+ipcMain.on("getData", (event,k ) => {
+    const value = store.get(k);
+    console.log('get result: ', value);
+    event.returnValue = value;
+    console.log("store path: ", storePath);
+})
+
+ipcMain.on("delData", (event,k ) => {
+    store.delete(k);
+    console.log('delete k successfully: ', k);
+})
