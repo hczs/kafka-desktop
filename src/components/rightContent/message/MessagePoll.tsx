0fcda9237cd0c6b6ee88786e28fbce872aa1da7a
houcheng <houcheng@boco.com.cn>
2023-10-09 18:28:38 +0800

    :sparkles: 消息拉取后支持排序，新增消息数量展示

--- src/components/rightContent/message/MessagePoll.tsx
+++ src/components/rightContent/message/MessagePoll.tsx
@@ -2,27 +2,23 @@ import React, {useEffect, useState} from 'react';
 import {Consumer, Kafka} from "kafkajs";
 import {
     Button,
-    Col,
     Form,
-    Grid,
     InputNumber,
     InputPicker,
     Message,
     Modal,
-    Row,
     Schema,
-    Stack,
-    Table,
+    Table, Tag,
     toaster,
     Toggle
 } from "rsuite";
 import "./MessagePoll.scss";
 import {FormInstance} from "rsuite/esm/Form/Form";
 import InfoOutlineIcon from "@rsuite/icons/InfoOutline";
-import TrashIcon from "@rsuite/icons/Trash";
 import {RowDataType} from "rsuite/Table";
 import ReactJson from 'react-json-view';
 import moment from "moment";
+import {SortType} from "rsuite-table/src/@types/common";
 
 const {Column, HeaderCell, Cell} = Table;
 
@@ -92,6 +88,14 @@ const MessagePoll = (props: Props) => {
     const [messageInfo, setMessageInfo] = useState<any>();
 
     const [msgInfoModalOpen, setMsgInfoModalOpen] = useState<boolean>(false);
+
+    // 当前排序列
+    const [sortColumn, setSortColumn] = React.useState<string>();
+
+    // 当前排序类型
+    const [sortType, setSortType] = React.useState<SortType>();
+
+    const [tableLoading, setTableLoading] = useState<boolean>(false);
 
     useEffect(() => {
         console.log("MessagePoll mount");
@@ -174,7 +178,7 @@ const MessagePoll = (props: Props) => {
                 msgConsumer.run({
                     autoCommit: false,
                     eachMessage: async (payload) => {
-                        console.log("payload: ", payload);
+                        // console.log("payload: ", payload);
                         if (payload.topic !== searchFormValue.topicName) {
                             return;
                         }
@@ -182,47 +186,56 @@ const MessagePoll = (props: Props) => {
                         let countFlag = false;
                         // 不是全部分区 则过滤指定分区的数据
                         if (!allPartition(partition) && payload.partition === partition) {
-                            console.log("[指定分区消费]消费到消息：", msg, " 分区：", payload.partition);
+                            // console.log("[指定分区消费]消费到消息：", msg, " 分区：", payload.partition);
                             countFlag = true;
                         } else if (allPartition(partition)) {
-                            console.log("[消费全部分区]消费到消息：", msg, " 分区：", payload.partition);
+                            // console.log("[消费全部分区]消费到消息：", msg, " 分区：", payload.partition);
                             countFlag = true;
                         }
                         if (countFlag) {
-                            setMessageList(oldList => [
-                                ...oldList,
-                                {
-                                    partitionId: payload.partition,
-                                    offset: msg.offset,
-                                    key: msg.key?.toString(),
-                                    value: msg.value?.toString(),
-                                    timestamp: moment(new Date(Number(msg.timestamp))).format("YYYY-MM-DD HH:mm:ss"),
-                                }
-                            ]);
+                            await processMessage(payload.partition, msg);
                         }
-                        console.log(new Date(msg.timestamp));
-                        moment(msg.timestamp).format("YYYY-MM-DD HH:mm:ss");
                         if (countFlag && count && --count == 0) {
                             stop();
                         }
                     }
                 }).then(() => {
-                    console.log("消费者已启动");
                     toaster.push(<Message showIcon type="success">消费者已启动</Message>, {
                         duration: 2000
                     });
                 });
-                console.log("partition: ", partition);
-                if (partition !== undefined && partition !== -1 && startOffset) {
-                    console.log("seek: ", topic, partition, startOffset);
+                // 选择分区 不是选择的全部分区 并且设置了起始offset 并且不是从头开始消费 设置消费开始的offset
+                if (partition !== undefined && partition !== -1 && startOffset && !searchFormValue.fromBeginning) {
                     msgConsumer.seek({
                         topic: topic, partition: partition,
                         offset: startOffset
+                    });
+                } else if (partition !== undefined && partition !== -1) {
+                    // 其他情况 单分区消费 需要重新seek offset
+                    msgConsumer.seek({
+                        topic: topic, partition: partition,
+                        offset: '0'
                     });
                 }
             })
         }
     }
+
+    const processMessage = async (partitionId: number, message: any) => {
+        setMessageList(oldList => [
+            ...oldList,
+            {
+                partitionId: partitionId,
+                offset: message.offset,
+                key: message.key?.toString(),
+                value: message.value?.toString(),
+                timestamp: moment(new Date(Number(message.timestamp))).format("YYYY-MM-DD HH:mm:ss"),
+            }
+        ]);
+
+        // 处理完消息后立即释放
+        await new Promise(resolve => setTimeout(resolve, 0));
+    };
 
     const pause = () => {
         msgConsumer?.pause([{topic: "test_2_3", partitions: [0]}]);
@@ -299,13 +312,58 @@ const MessagePoll = (props: Props) => {
         setMsgInfoModalOpen(false);
     }
 
+    const sortTableData = (dataKey: string, sortType?: SortType) => {
+        if (consuming) {
+            toaster.push(<Message showIcon type="info">请先停止消费者，再进行排序操作</Message>, {
+                duration: 2000
+            });
+            return;
+        }
+        setTableLoading(true);
+        setSortColumn(dataKey);
+        setSortType(sortType);
+        const numberKey = ["partitionId", "offset"];
+        console.log(dataKey + sortType);
+        setMessageList(messageList.sort((msg1, msg2) => {
+            const fieldKey = dataKey as keyof typeof msg1;
+            let v1 = msg1[fieldKey];
+            let v2 = msg2[fieldKey];
+            if (numberKey.indexOf(dataKey) >= 0) {
+                v1 = v1 as number;
+                v2 = v2 as number;
+                // 解决 sort 0 干扰
+                if (v1 == 0 && v2 != 0) {
+                    v1 += 1;
+                    v2 += 1;
+                }
+                if (v2 == 0 && v1 != 0) {
+                    v2 += 1;
+                    v1 += 1;
+                }
+                if (sortType === "desc" && v1 && v2) {
+                    return v2 - v1;
+                } else {
+                    return v1 - v2;
+                }
+            } else if (dataKey === "timestamp" && v1 && v2) {
+                if (sortType === "desc") {
+                    return Date.parse(v2 as string) - Date.parse(v1 as string);
+                } else {
+                    return Date.parse(v1 as string) - Date.parse(v2 as string);
+                }
+            }
+            return 0;
+        }));
+        setTableLoading(false);
+    }
+
     return (
         <div className="message-poll-container">
             <Modal
-                   keyboard={false}
-                   open={msgInfoModalOpen}
-                   onClose={handleMsgInfoModalClose}
-                   size={"lg"}
+                keyboard={false}
+                open={msgInfoModalOpen}
+                onClose={handleMsgInfoModalClose}
+                size={"lg"}
             >
                 <Modal.Header>
                     <Modal.Title>消息详情</Modal.Title>
@@ -382,7 +440,7 @@ const MessagePoll = (props: Props) => {
                 <Form.Group controlId="offset" className={"group-width"}>
                     <Form.ControlLabel className={"label-width"}>Offset</Form.ControlLabel>
                     <Form.Control
-                        disabled={!(searchFormValue.partitionId != -1 && !searchFormValue.fromBeginning)}
+                        disabled={!(searchFormValue.partitionId != -1 && !searchFormValue.fromBeginning) || consuming}
                         className={"input-width"}
                         name="offset"
                         accepter={InputNumber}
@@ -410,16 +468,20 @@ const MessagePoll = (props: Props) => {
                 </div>
             </Form>
             <Table
+                onSortColumn={sortTableData}
+                sortColumn={sortColumn}
+                sortType={sortType}
                 fillHeight
                 bordered={false}
                 cellBordered={false}
                 data={messageList}
+                loading={tableLoading}
             >
-                <Column align="center" flexGrow={1}>
+                <Column align="center" flexGrow={1} sortable>
                     <HeaderCell>分区</HeaderCell>
                     <Cell dataKey="partitionId"/>
                 </Column>
-                <Column align="center" flexGrow={1}>
+                <Column align="center" flexGrow={1} sortable>
                     <HeaderCell>Offset</HeaderCell>
                     <Cell dataKey="offset"/>
                 </Column>
@@ -431,7 +493,7 @@ const MessagePoll = (props: Props) => {
                     <HeaderCell>Value</HeaderCell>
                     <Cell dataKey="value"/>
                 </Column>
-                <Column align="center" width={200}>
+                <Column align="center" width={200} sortable>
                     <HeaderCell>Timestamp</HeaderCell>
                     <Cell dataKey="timestamp"/>
                 </Column>
@@ -450,6 +512,7 @@ const MessagePoll = (props: Props) => {
                     </Cell>
                 </Column>
             </Table>
+            <Tag color="blue" style={{marginTop: "10px"}}>当前数据条数：{messageList.length}</Tag>
         </div>
     );
 };
