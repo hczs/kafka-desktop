f4c0c96987906a5687d24b8e0483c2bf8596484b
sunny <hczshd@163.com>
2023-10-10 23:26:53 +0800

    :green_heart: 添加自动构建后处理脚本，减轻构建包体积

---
+++ handle_pkg.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+// 获取版本号信息
+let appVersion = null;
+let appName = null;
+fs.readFile('./package.json', 'utf-8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    const pkg = JSON.parse(data.toString());
+    appVersion = pkg.version;
+    appName = pkg.name;
+
+    if (appVersion == null) {
+        throw "版本号解析失败";
+    }
+
+    if (appName == null) {
+        throw "应用名称解析失败";
+    }
+
+    // 创建 ./release/installation-packages 文件夹
+    let targetPath = './release/installation-packages/';
+    fs.mkdirSync(targetPath);
+
+    // 从 ./release/${version} 文件夹下取出安装包 放到 ./release/installation-packages 下
+    let commonPath = "./release/" + appVersion + "/";
+    let fileName = appName + "_" + appVersion;
+    let winSourceFile = commonPath + fileName + ".exe";
+    let macSourceFile = commonPath + fileName + ".dmg";
+
+    // 复制文件
+    fs.copyFileSync(winSourceFile, targetPath + fileName + ".exe");
+    fs.copyFileSync(macSourceFile, targetPath + fileName + ".dmg");
+});
+
+
